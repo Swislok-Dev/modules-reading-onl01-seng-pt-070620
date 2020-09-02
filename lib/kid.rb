ccb7fa2db2c84b212306da1a3aa22ee131d3ce0b
Swislok-Dev <cmarker4@gmail.com>
2020-09-02 18:50:15 -0500

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
 
 class Kid
+
+    extend FancyDance::ClassMethods 
+    include FancyDance::InstanceMethods 
+
 
     include Dance 
     extend MetaDancing
